902e8f6542a7a3f33f7ee544a1f09048ffcdafaf
rymac <mach@rMacBook-Pro-2.local>
2013-09-02 06:36:44 +0900

    ./a.out crossing.txt  31.53s user 0.01s system 99% cpu 31.535 total

---
+++ crossing1.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    FILE *fp;
+    int c;
+    int n_data = 0;
+    fp  = fopen(argv[1], "r");
+    while ((c = getc(fp)) != EOF) {
+        //        printf("%c",(char)c);
+        if (c == '\n')
+            n_data++;
+    }
+    fclose(fp);
+
+    int *d;
+    long cross = 0;
+    int i, j;
+    d = (int *)malloc((n_data + 1) * sizeof(int));
+    fp  = fopen(argv[1], "r");
+    for (i = 1; i <= n_data; i++) {
+        fscanf(fp, "%d", &d[i]);
+    }
+    fclose(fp);
+
+    for (i = 1; i <= n_data; i++) {
+        //        printf("%d/%d\n", i, n_data);
+        if (d[i] == i)
+            continue;
+
+        int prev = d[i];
+        for (j = i + 1; j <= n_data; j++) {
+            if (d[j] == i) {
+                cross += j - i;
+                d[j] = prev;
+            } else {
+                int tmp = d[j];
+                d[j] = prev;
+                prev = tmp;
+            }
+        }
+    }
+
+    printf("# of crossing point = %ld\n", cross);
+
+    free(d);
+    return 0;
+}
+
